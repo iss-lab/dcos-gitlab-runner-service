da6a046559fce8cb988f300400aafbe8118f6c78
srajappa <srajappa@buffalo.edu>
2019-09-09 13:00:02 -0400

    fix: - not parameterizing the private key.

--- ensure_dcos_login.sh
+++ ensure_dcos_login.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-dcos node || echo "Stale login found, attempting to authenticate" && dcos auth login --username $SERVICE_PRINCIPAL --private-key $RUNNER_SECRET && dcos node && echo "Authentication Succeded" || echo "Authentication failed, please review the configured secret for runner"
+dcos node || echo "Stale login found, attempting to authenticate" && dcos auth login --username $SERVICE_PRINCIPAL --private-key /gitlab-runner-private.pem && dcos node && echo "Authentication Succeded" || echo "Authentication failed, please review the configured secret for runner"
